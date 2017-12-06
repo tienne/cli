3249f5b2b5848daf0e08f733c4df30db5095d60e
davidKwon <tienne89@gmail.com>
2017-12-06 16:42:06 +0900

    clime 로 넘기는 부분 async로 적용

--- dist/cli/index.js
+++ dist/cli/index.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("source-map-support/register");
 const path = require("path");
 const clime_1 = require("clime");
 const cli = new clime_1.CLI('minda', path.join(__dirname, 'commands'));
 const shim = new clime_1.Shim(cli);
-shim.execute(process.argv).then(() => {
-    process.exit(0);
-}).catch((error) => {
-    console.log(error);
-    process.exit(-1);
-});
+(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield shim.execute(process.argv).then(() => {
+            //
+        }).catch((error) => {
+            console.log(error);
+            process.exit(-1);
+        });
+        process.exit(0);
+    });
+})();
 //# sourceMappingURL=index.js.map
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,11 +5,15 @@ import * as path from 'path';
 import { CLI, Shim } from 'clime';
 
 const cli = new CLI('minda', path.join(__dirname, 'commands'));
-
 const shim = new Shim(cli);
-shim.execute(process.argv).then(() => {
+
+(async function() {
+  await shim.execute(process.argv).then(() => {
+    //
+  }).catch((error) => {
+    console.log(error);
+    process.exit(-1);
+  });
+
   process.exit(0);
-}).catch((error) => {
-  console.log(error);
-  process.exit(-1);
-});
+})();
